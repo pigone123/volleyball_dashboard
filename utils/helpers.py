9fdcc6080b6d50b613ca85adf1c1327133e9139a
pigone123 <galking123ster@gmail.com>
2026-01-07 21:57:56 +0200

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import arabic_reshaper
 
 def horizontal_radio(label, options, session_key):
     current = st.session_state.get(session_key, options[0])
@@ -12,3 +13,9 @@ def horizontal_radio(label, options, session_key):
 
 def extract_category(event_value):
     return event_value.split("(")[0].strip() if "(" in event_value else event_value
+
+
+def rtl(text):
+    if not isinstance(text, str):
+        return text
+    return get_display(arabic_reshaper.reshape(text))
